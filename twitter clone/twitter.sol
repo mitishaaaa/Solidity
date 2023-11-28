56489cfdf994bca3dd9987ca7329949f884a8cc0
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-11-28 23:54:00 +0530

    Update twitter.sol

--- twitter clone/twitter.sol
+++ twitter clone/twitter.sol
@@ -1,4 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+//1. create a twitter contract
+//2. create a mapping between user and tweet
+//3. add function to create a tweet and save it in mapping
+//4. create a function to get tweet
+//1.
+contract Twitter{
+//2.
+mapping(address => string) public tweets;
+//3. func to create the tweet 
+function createTweet(string memory _tweet) public 
+     tweets[msg.sender] = _tweet; 
+     //saving the tweet
+     }
 
      
 
