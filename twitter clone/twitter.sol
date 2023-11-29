01623d5badb4ebc908c3fde8d4f0963ca40e077b
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-11-29 23:25:16 +0530

    Update twitter.sol

--- twitter clone/twitter.sol
+++ twitter clone/twitter.sol
@@ -7,14 +7,24 @@ pragma solidity ^0.8.0;
 //4. create a function to get tweet
 //1.
 contract Twitter{
+
 //2.
 mapping(address => string) public tweets;
+
 //3. func to create the tweet 
 function createTweet(string memory _tweet) public 
      tweets[msg.sender] = _tweet; 
      //saving the tweet
      }
 
+//4.
+function getTweet(address _owner) public view returns (string memory)
+//u have to specify what type u are returning, therefore string is used. 
+//memory is used bc u have to store certain things.
+//view is specified cos its simply returning some data and allows sol to be more gas efficient.
+{
+return tweets[_owner]; //finding thats why square bracket
+}
      
 
 
