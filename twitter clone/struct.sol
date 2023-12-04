f47d010d3b3505adf0f59a2a9dabdec90513f300
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-12-04 23:41:10 +0530

    Create struct.sol

---
+++ twitter clone/struct.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//1️⃣ Define a Tweet struct with author, content, timestamp, likes.
+//2️⃣ Add the struct to array.
+//3️⃣ Test Tweets.
+
+contract Twitter {
+ //1️⃣
+ struct Tweet {
+ address author;
+ string content;
+ uint256 timestamp;
+ uint256 likes;
+ }
+
+//2️⃣
+mapping(address => Tweet[]) public tweets;
+ function createTweet(string memory _tweet) public{
+ //memory =  temp storage till the task finishes
+   Tweet memory newTweet = Tweet({ 
+      author: msg.sender,
+      content: _tweet,
+      timestamp: block.timestamp,
+      likes: 0
+      });
+
+      tweets[msg.sender].push(newTweet);
+      }
+
+  function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
+  return tweets[_owner][_i];
+  }
+
+  function getAllTweets(address _owner) public view returns (Tweet[] memory){
+  return tweets[_owner];
+  }
+  }
